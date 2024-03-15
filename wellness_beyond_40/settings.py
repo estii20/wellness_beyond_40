9592051b5f8a6698fcc12822869fb0135d60b0fd
estii20 spe <estellespecht@hotmail.co.uk>
2024-03-15 15:40:24 +0000

    Add comment success message to views.py

--- wellness_beyond_40/settings.py
+++ wellness_beyond_40/settings.py
@@ -11,8 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+from django.contrib.messages import constants as messages
 import dj_database_url
+import os
 if os.path.isfile('env.py'):
     import env
 
@@ -57,7 +58,15 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-CRISPY_TEMPLATE_PACK - 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-info',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+    }
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
